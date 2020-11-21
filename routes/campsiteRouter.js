adcae9d6bd81d829448f180f0baa4c8813a3b979
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-11-21 10:23:20 -0500

    update campsiteRouter

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -24,4 +24,26 @@ campsiteRouter
     res.end('Deleting all campsites');
   });
 
+campsiteRouter
+  .route('/:campsiteId')
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Will get the campsite: ${req.params.campsiteId}`);
+  })
+  .post((req, res) => {
+    res.end(`Will add the campsite: ${req.params.campsiteId}`);
+  })
+  .put((req, res) => {
+    res.end(
+      `Will update the campsite: ${req.params.campsiteId} - ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .delete((req, res) => {
+    res.end(`Will delete the campsite: ${req.params.campsiteId}`);
+  });
+
 module.exports = campsiteRouter;
